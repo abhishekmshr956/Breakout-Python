f69024089c56bb8ddcb5005bb8728a31625008c3
abhishekmshr956 <abhishekmshr956@gmail.com>
2023-08-16 23:38:05 -0700

    first commit

---
+++ code/main.py
@@ -0,0 +1,43 @@
+import pygame, sys, time
+from settings import *
+
+class Game:
+    def __init__(self):
+
+        # general setup
+        pygame.init()
+        self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        pygame.display.set_caption('Breakout')
+
+        # background
+        self.bg = self.create_bg()
+
+    def create_bg(self):
+        bg_original = pygame.image.load('../graphics/other/bg.png').convert()
+        scaled_bg = pygame.transform.scale(bg_original, (WINDOW_WIDTH, WINDOW_HEIGHT))
+        return scaled_bg
+
+    def run(self):
+        last_time = time.time()
+        while True:
+
+            # delta time
+            dt = time.time() - last_time
+            last_time = time.time()
+
+            # event loop
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            # draw the frame
+            self.display_surface.blit(self.bg,(0,0))
+
+            # update window
+            pygame.display.update()
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
+
