afd07da6769e23d754fca5c9fbb859fa7d5ceeb9
abhishekmshr956 <abhishekmshr956@gmail.com>
2023-08-17 11:30:36 -0700

    blocks added

--- code/main.py
+++ code/main.py
@@ -1,6 +1,6 @@
 import pygame, sys, time
 from settings import *
-from sprites import Player, Ball
+from sprites import Player, Ball, Block
 
 class Game:
     def __init__(self):
@@ -15,10 +15,13 @@ class Game:
 
         # sprite group setup
         self.all_sprites = pygame.sprite.Group()
+        self.block_sprites = pygame.sprite.Group()
 
         # setup
         self.player = Player(self.all_sprites)
-        self.ball = Ball(self.all_sprites, self.player)
+        self.stage_setup()
+        self.ball = Ball(self.all_sprites, self.player, self.block_sprites)
+        
 
     def create_bg(self):
         bg_original = pygame.image.load('../graphics/other/bg.png').convert()
@@ -27,6 +30,18 @@ class Game:
         scaled_width = bg_original.get_width() * scale_factor
         scaled_bg = pygame.transform.scale(bg_original, (scaled_width, scaled_height))
         return scaled_bg
+    
+    def stage_setup(self):
+        # cycle through all rows and columns of BLOCK MAP
+        for row_index, row in enumerate(BLOCK_MAP):
+            for col_index, col in enumerate(row):
+                if col != ' ':
+                    # find the x and y position for each individual block
+                    x = col_index * (BLOCK_WIDTH + GAP_SIZE) + GAP_SIZE // 2
+                    y = row_index * (BLOCK_HEIGHT + GAP_SIZE) + GAP_SIZE // 2
+                    Block(col, (x, y), [self.all_sprites, self.block_sprites])
+            
+        
 
     def run(self):
         last_time = time.time()
